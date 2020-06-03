d8f96443d2ef2741bcc7313fc54b990990f80017
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-02 22:05:22 -0300

    refactor: move input to a separate file

---
+++ src/presentation/components/input/index.tsx
@@ -0,0 +1,20 @@
+import React, { memo, InputHTMLAttributes } from 'react'
+
+import Styles from './styles.scss'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  type: string
+  name?: string
+  placeholder?: string
+}
+
+const Input: React.FC<InputProps> = ({ ...props }) => {
+  return (
+    <div className={Styles.inputWrap}>
+      <input { ...props} />
+      <span className={Styles.status}>🔴</span>
+    </div>
+  )
+}
+
+export default memo(Input)
